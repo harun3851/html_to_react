eac4568ed849a132173e48af04edef516b659ea7
Harun Koyuncu <30460243+harun3851@users.noreply.github.com>
2023-02-17 01:22:42 +0200

    slider added

---
+++ src/components/Carousel.js
@@ -0,0 +1,34 @@
+import React, { useState } from 'react';
+import "./Carousel.css";
+import { images } from "../helpers/CarouselData";
+import ArrowBackIosIcon  from '@mui/icons-material/ArrowBackIos';
+import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
+function Carousel() {
+    const [currImg, setCurrImg] = useState(1);
+  return (
+    <div className='carousel'>
+        <div className='carouselInner' style={{backgroundImage:`url(${images[currImg].img})`}}>
+    
+        <div className='left' onClick={()=>{
+          currImg > 0 && setCurrImg(currImg-1);
+        }}>
+        <ArrowBackIosIcon />
+        </div>
+    
+        <div className='center'>
+          <h1>{images[currImg].title}</h1>
+          <p>{images[currImg].subtitle}</p>
+        </div>
+
+        <div className='right' onClick={()=>{
+           currImg < images.length-1 && setCurrImg(currImg+1);
+        }}>
+          <ArrowForwardIosIcon />
+        </div>
+        </div>
+
+    </div>
+  );
+}
+
+export default Carousel;
---
+++ src/helpers/CarouselData.js
@@ -0,0 +1,12 @@
+import Img1 from "../images/img-1.jpg";
+import Img2 from "../images/img-2.jpg";
+import Img3 from "../images/img-3.jpg";
+export const images=[
+    {
+        title:"first",subtitle:"first stitle", img:Img1},
+    {
+        title:"second",subtitle:"second stitle", img:Img2},
+    {
+        title:"third",subtitle:"third stitle", img:Img3},
+    
+];
