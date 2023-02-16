1f057fc63c7774f12df9702fb9b57265e2668c31
Harun Koyuncu <30460243+harun3851@users.noreply.github.com>
2023-02-17 01:39:22 +0200

    set timer for slider

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -1,10 +1,17 @@
-import React, { useState } from 'react';
+import React, {useEffect, useState } from 'react';
 import "./Carousel.css";
 import { images } from "../helpers/CarouselData";
 import ArrowBackIosIcon  from '@mui/icons-material/ArrowBackIos';
 import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
 function Carousel() {
     const [currImg, setCurrImg] = useState(1);
+    useEffect(() => {
+      const timer = setTimeout(() => {
+        setCurrImg(prevCurrImg => (prevCurrImg === images.length - 1 ? 0 : prevCurrImg + 1));
+      }, 10000);
+    
+      return () => clearTimeout(timer);
+    }, [currImg, images.length]);
   return (
     <div className='carousel'>
         <div className='carouselInner' style={{backgroundImage:`url(${images[currImg].img})`}}>
